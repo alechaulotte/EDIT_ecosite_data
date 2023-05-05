71f954af0d92241789570e770576b30249c80c3f
Haulotte <alec@alechaulotte.com>
2023-05-05 11:01:17 -0500

    output_directory param

--- R/NASIS_EDIT_single.R
+++ R/NASIS_EDIT_single.R
@@ -3,12 +3,12 @@
 #' @name NASIS_EDIT_single
 #' 
 #' @param ecosite ecosite code of interest
-#' @param output_directory output directory for report (optional, will default output to package folder)
+#' @param output_directory output directory for report 
 #' 
 #' @return html report
 #' @export
 
-NASIS_EDIT_single <- function(ecosite, output_directory = getwd()) {
+NASIS_EDIT_single <- function(ecosite, output_directory) {
   rmarkdown::render(
     system.file("rmd", "NASIS_data_to_EDIT_single.Rmd", package = "soilutils"), 
     ourput_dir = output_directory,
