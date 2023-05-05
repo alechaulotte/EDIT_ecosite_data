7df823b79f309ac1c064d3112bfdfe875c4cc04e
Haulotte <alec@alechaulotte.com>
2023-05-05 13:56:44 -0500

    add data path param, remove default output dir

--- R/NASIS_EDIT_single.R
+++ R/NASIS_EDIT_single.R
@@ -2,18 +2,20 @@
 #' 
 #' @name NASIS_EDIT_single
 #' 
-#' @param ecosite ecosite code of interest
-#' @param output_directory output directory for report (will default to wd)
+#' @param ecosite_id ecosite code of interest
+#' @param data_path path to ecosite_report.csv (generated from NASIS Report)
+#' @param output_directory output directory for report
 #' 
-#' @return html report
+#' @return interactive html report
 #' @export
 
-NASIS_EDIT_single <- function(ecosite, output_directory = getwd()) {
+NASIS_EDIT_single <- function(ecosite_id, data_path, output_directory) {
   rmarkdown::render(
     system.file("rmd", "NASIS_data_to_EDIT_single.Rmd", package = "editdata"), 
     output_dir = output_directory,
     params = list(
-      ecosite = ecosite
+      ecosite = ecosite_id,
+      data = data_path
     )
   )
   
