ead8020ac3751c73b231e69a5f185371cb01c04a
Haulotte <alec@alechaulotte.com>
2023-05-05 14:27:57 -0500

    param refs

--- R/NASIS_EDIT_single.R
+++ R/NASIS_EDIT_single.R
@@ -3,13 +3,13 @@
 #' @name NASIS_EDIT_single
 #' 
 #' @param data_path path to ecosite_report.csv (generated from NASIS Report)
-#' @param ecosite ecosite code of interest
+#' @param ecosite_id ecosite code of interest
 #' @param output_directory output directory for report
 #' 
 #' @return interactive html report
 #' @export
 
-NASIS_EDIT_single <- function(data_path, ecosite, output_directory) {
+NASIS_EDIT_single <- function(data_path, ecosite_id, output_directory) {
   rmarkdown::render(
     system.file("rmd", "NASIS_data_to_EDIT_single.Rmd", package = "editdata"), 
     output_dir = output_directory,
